47adfaee8ea4453403cdb7d02fe293415a993fe2
Bárbara Oliveira da Silva <barbara@ciandt.com>
2020-07-29 19:12:04 -0300

    usando layout builder

--- lib/main.dart
+++ lib/main.dart
@@ -89,10 +89,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     final appBar = AppBar(
-      title: Text(
-        "Despesas Pessoais",
-        style: TextStyle(fontSize: 20 * MediaQuery.of(context).textScaleFactor),
-      ),
+      title: Text("Despesas Pessoais"),
       actions: [
         IconButton(
           icon: Icon(
